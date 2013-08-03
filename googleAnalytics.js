1abcdb26528b0adb329b588024c6d7ea88b92ed1
Phuoc Do <dnprock@gmail.com>
2013-08-02 18:35:56 -0700

    Specify domain name

--- googleAnalytics.js
+++ googleAnalytics.js
@@ -1,15 +1,19 @@
 if (Meteor.isClient) {
   Template.googleAnalytics.rendered = function() {
-      new GA('UA-42923639-1', 'leaderboard');
+      new GA();
   }
  
-  GA = function(code, account) {
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
- 
-      ga('create', code, account);
-      ga('send', 'pageview');
+  GA = function(){
+    if (window._gaq) return;
+    window._gaq = window._gaq || [];
+    _gaq.push(['_setAccount', 'UA-42923639-1']);
+    _gaq.push(['_setDomainName', 'meteor.com']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
   };
 }
